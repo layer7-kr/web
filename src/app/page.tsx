584812204176ec312c8e3c448e000e10b3a2c827
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-04 16:29:45 +0900

    feat: add Layer7Symbol component with colored and grayscale options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Layer7Symbol from '@/components/Layer7Symbol';
+
 export default function Home() {
-  return <h1>Test</h1>;
+  return (
+    <div>
+      <Layer7Symbol size={100} />
+    </div>
+  );
 }
---
+++ src/components/Layer7Symbol/index.tsx
@@ -0,0 +1,40 @@
+import cn from 'classnames';
+import Logo from './assets/logo.svg';
+import LogoGrayscale from './assets/logo_grayscale.svg';
+
+import * as s from './styles.css';
+
+interface Layer7Symbol {
+  colored?: boolean;
+  size: number;
+  className?: string;
+  onClick?: () => void;
+}
+
+export default function Layer7Symbol(props: Layer7Symbol) {
+  const { colored = false, size, className, onClick } = props;
+
+  const LogoComponent = colored ? Logo : LogoGrayscale;
+
+  const renderedIcon = (
+    <LogoComponent
+      className={cn(className, s.base)}
+      width={size}
+      height={size}
+      style={{
+        minWidth: size,
+        minHeight: size,
+      }}
+    />
+  );
+
+  if (onClick) {
+    return (
+      <div className={s.clickable} onClick={onClick}>
+        {renderedIcon}
+      </div>
+    );
+  } else {
+    return renderedIcon;
+  }
+}
