e559d9647cc36129e03f3336889c63e3bc3fc829
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-04 18:18:56 +0900

    feat: add HomeHeroSection component and integrate it into the Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import HomeAboutSection from '@/sections/home/About';
+import HomeHeroSection from '@/sections/home/Hero';
 import * as s from './page.css';
 
 export default function Home() {
-  return <div className={s.base}>
-    <HomeAboutSection />
-  </div>;
+  return (
+    <div className={s.base}>
+      <HomeHeroSection />
+      <HomeAboutSection />
+    </div>
+  );
 }
