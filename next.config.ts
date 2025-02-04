6e690009d472b8270f3c75851097f568b57ce72b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-04 16:21:45 +0900

    feat: add SVGR support for SVG imports and configure path aliasing

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,26 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
+import path from 'path';
 
-const withVanillaExtract = createVanillaExtractPlugin();
+const withVanillaExtract = createVanillaExtractPlugin({
+  identifiers: 'short',
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+
+    return config;
+  },
 };
 
 export default withVanillaExtract(nextConfig);
