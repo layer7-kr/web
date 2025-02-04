93de6fc07b112ac70d3498b17f7e33c6ec30040a
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-04 17:46:37 +0900

    feat: add LexendMega font and ensure proper application of global font faces

--- src/styles/fonts/index.css.ts
+++ src/styles/fonts/index.css.ts
@@ -2,6 +2,8 @@ import { applyGlobalFontFace } from '@/lib/utils/styles/font';
 
 export const pretendard = 'Pretendard';
 export const montserrat = 'Montserrat';
+export const lexendMega = 'LexendMega';
 
 applyGlobalFontFace(pretendard, 'woff2');
+applyGlobalFontFace(montserrat, 'ttf');
 applyGlobalFontFace(montserrat, 'ttf');
