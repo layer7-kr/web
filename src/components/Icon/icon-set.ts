8041b8f04ee4ef95e9528633b3ca7532674f7a89
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-04 19:11:19 +0900

    feat: add ArrowForward icon and update icon set to include it

--- src/components/Icon/icon-set.ts
+++ src/components/Icon/icon-set.ts
@@ -1,9 +1,12 @@
+import ArrowForward from './assets/arrow_forward.svg';
 import KeyboardArrowDown from './assets/keyboard_arrow_down.svg';
 
 export enum IconName {
   KEYBOARD_ARROW_DOWN = 'keyboard_arrow_down',
+  ARROW_FORWARD = 'arrow_forward',
 }
 
 export const IconNameMap = {
   [IconName.KEYBOARD_ARROW_DOWN]: KeyboardArrowDown,
+  [IconName.ARROW_FORWARD]: ArrowForward,
 } as const;
