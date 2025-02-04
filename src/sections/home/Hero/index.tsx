7e3bb37ed0681f990dc488be4e965f1ac72f1bbe
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-04 19:11:09 +0900

    feat: implement HomeHeroSection with branding, title, and application button

--- src/sections/home/Hero/index.tsx
+++ src/sections/home/Hero/index.tsx
@@ -1,5 +1,48 @@
+import Icon from '@/components/Icon';
+import { IconName } from '@/components/Icon/icon-set';
+import Layer7Symbol from '@/components/Layer7Symbol';
+import Typo from '@/components/Typo';
+import { lexendMega } from '@/styles/fonts/index.css';
+import { weight } from '@/styles/fonts/values/weight';
 import * as s from './style.css';
 
 export default function HomeHeroSection() {
-  return <section className={s.base}></section>;
+  return (
+    <section className={s.base}>
+      <div className={s.container}>
+        <div className={s.branding}>
+          <Layer7Symbol size={136} />
+          <div className={s.info}>
+            <div className={s.title}>
+              <Typo
+                family={lexendMega}
+                size={76}
+                weight={weight.black}
+                className={s.titleLine}
+                color={'#047ee9'}>
+                we hack the
+              </Typo>
+              <Typo
+                family={lexendMega}
+                size={76}
+                weight={weight.black}
+                className={s.titleLine}
+                color={'#fff'}>
+                universe
+              </Typo>
+            </div>
+            <Typo size={18} weight={weight.extrabold} color={'#fff'}>
+              선린인터넷고등학교 해킹 전문 동아리 Layer7
+            </Typo>
+          </div>
+        </div>
+        <button className={s.apply}>
+          <Typo size={18} weight={weight.bold} color={'#fff'}>
+            동아리 지원하기
+          </Typo>
+          <Icon name={IconName.ARROW_FORWARD} size={20} color={'#fff'} />
+        </button>
+      </div>
+    </section>
+  );
 }
--- src/sections/home/Hero/style.css.ts
+++ src/sections/home/Hero/style.css.ts
@@ -1,6 +1,66 @@
+import { flexCenter, flexColumn } from '@/lib/utils/styles/flex';
 import { style } from '@vanilla-extract/css';
 
 export const base = style({
   width: '100%',
   height: '100vh',
+  background: 'rgba(0, 0, 0, 0.7)',
+  backgroundImage: 'url(/assets/background.png)',
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  ...flexCenter(),
+});
+
+export const container = style({
+  ...flexColumn({
+    align: 'center',
+    gap: 80,
+  }),
+});
+
+export const branding = style({
+  ...flexColumn({
+    align: 'center',
+    gap: 4,
+  }),
+});
+
+export const info = style({
+  ...flexColumn({
+    align: 'center',
+    gap: 20,
+  }),
+});
+
+export const title = style({
+  textTransform: 'uppercase',
+  ...flexColumn({
+    align: 'center',
+    gap: 0,
+  }),
+});
+
+export const titleLine = style({
+  letterSpacing: '-14%',
+  lineHeight: 1,
+  mixBlendMode: 'difference',
+});
+
+export const apply = style({
+  cursor: 'pointer',
+  padding: '16px 30px',
+  ...flexCenter({
+    gap: 10,
+  }),
+  width: 'fit-content',
+  borderRadius: 42,
+  border: '2px solid #fff',
+  background: 'rgba(255, 255, 255, 0.11)',
+  transition: 'background 0.1s ease-in-out',
+  ':hover': {
+    background: 'rgba(255, 255, 255, 0.4)',
+  },
+  ':active': {
+    background: 'rgba(255, 255, 255, 0.3)',
+  },
 });
