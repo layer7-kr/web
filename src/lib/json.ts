b29033596a8ab6e0270eb7d67cdce998196c708c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-05 11:47:41 +0900

    refactor: improve formatting and readability of JSON file reading function

--- src/lib/json.ts
+++ src/lib/json.ts
@@ -1,4 +1,3 @@
-import { JSONActivityData } from '@/types/json';
 import { promises as fs } from 'fs';
 import path from 'path';
 
@@ -8,7 +7,7 @@ import path from 'path';
  * @param {string} sourcePath - public/data 디렉토리 기준 JSON 파일 경로
  * @returns {Promise<T>} 파싱된 JSON 데이터를 포함한 Promise
  * @throws {Error} 파일 읽기 또는 파싱 실패시 에러 발생
- * 
+ *
  * @example
  * // 활동 데이터 가져오기
  * const activities = await getJSON<ActivityDataList>('_activity.json');
@@ -16,7 +15,10 @@ import path from 'path';
 export async function getJSON<T>(sourcePath: string): Promise<T> {
   try {
     const jsonDirectory = path.join(process.cwd(), 'public/data');
-    const fileContents = await fs.readFile(path.join(jsonDirectory, sourcePath), 'utf8');
+    const fileContents = await fs.readFile(
+      path.join(jsonDirectory, sourcePath),
+      'utf8',
+    );
     return JSON.parse(fileContents) as T;
   } catch (error) {
     console.error('Error reading JSON:', error);
