02edf6c808d54219cd3056c31cb0da256fa1c3d6
iamfiro <appturbo102@gmail.com>
2025-02-06 19:44:06 +0900

    feat: restructure layout for Home section with new details wrapper and improved styling

--- src/sections/home/Activity/style.css.ts
+++ src/sections/home/Activity/style.css.ts
@@ -2,6 +2,8 @@ import { flexBetween, flexCenter } from "@/lib/utils/styles/flex";
 import { style } from "@vanilla-extract/css";
 
 export const base = style({
+    width: '100%',
+    
     margin: '0 auto',
     padding: '50px 0',
     
