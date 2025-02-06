d36c06c25cbdca77c5acf508be56f3fc327410bd
iamfiro <appturbo102@gmail.com>
2025-02-06 19:46:30 +0900

    feat: enhance Activity section styling with responsive grid layout

--- src/sections/home/Activity/style.css.ts
+++ src/sections/home/Activity/style.css.ts
@@ -3,7 +3,7 @@ import { style } from "@vanilla-extract/css";
 
 export const base = style({
     width: '100%',
-    
+
     margin: '0 auto',
     padding: '50px 0',
     
@@ -17,5 +17,11 @@ export const activityList = style({
     width: '100%',
 
     display: 'grid',
-    gridTemplateColumns: 'repeat(2, 1fr)'
+    gridTemplateColumns: 'repeat(2, 1fr)',
+
+    '@media': {
+        '(max-width: 768px)': {
+            gridTemplateColumns: '1fr',
+        }
+    }
 })
