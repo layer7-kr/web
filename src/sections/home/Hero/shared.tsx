8f9ebe06601ae068dbe19e7392e73cf7fa8c2c61
iamfiro <appturbo102@gmail.com>
2025-02-17 13:00:12 +0900

    feat: update typography sizes and letter spacing in Hero components for improved readability

--- src/sections/home/Hero/shared.tsx
+++ src/sections/home/Hero/shared.tsx
@@ -7,7 +7,7 @@ import { lexendMega } from "@/styles/fonts/index.css";
 import * as s from "./style.css";
 
 import { PropsWithChildren } from "react";
-import { IconName } from '@/components/Icon/icon-set';
+import { IconName } from "@/components/Icon/icon-set";
 import Icon from "@/components/Icon";
 
 interface HeroTitleProps {
@@ -19,16 +19,15 @@ export const HeroTitle = ({ children, color = "#ffffff" }: HeroTitleProps) => {
     <Typo
       family={lexendMega}
       size={{
-        450: 34,
-        600: 48,
-        768: 62,
+        480: 34,
+        768: 52,
         default: 76,
       }}
       weight={weight.black}
       className={s.titleLine}
       letterSpacing={{
-        450: "-3px",
-        600: "-7px",
+        480: "-2px",
+        768: "-6px",
         default: "-10px",
       }}
       color={color}
@@ -40,8 +39,14 @@ export const HeroTitle = ({ children, color = "#ffffff" }: HeroTitleProps) => {
 
 export const HeroDescription = ({ children }: PropsWithChildren) => {
   return (
-    <Typo size={{450: 15,
-      default: 18,}} weight={weight.semibold} color={"#ffffff"}>
+    <Typo
+      size={{
+        480: 15,
+        default: 18,
+      }}
+      weight={weight.semibold}
+      color={"#ffffff"}
+    >
       {children}
     </Typo>
   );
@@ -50,13 +55,17 @@ export const HeroDescription = ({ children }: PropsWithChildren) => {
 export const ApplyButton = ({ children }: PropsWithChildren) => {
   return (
     <div className={s.apply}>
-      <Typo size={{
-        768: 15,
-        default: 18,
-      }} weight={weight.semibold} color={"#ffffff"}>
+      <Typo
+        size={{
+          768: 15,
+          default: 18,
+        }}
+        weight={weight.semibold}
+        color={"#ffffff"}
+      >
         {children}
       </Typo>
       <Icon name={IconName.ARROW_FORWARD} size={18} color={"#ffffff"} />
     </div>
   );
-}
+};
