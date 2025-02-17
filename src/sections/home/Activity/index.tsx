1fa1c1db37f900a975fe093d2b82c80fb51f0d8c
iamfiro <appturbo102@gmail.com>
2025-02-17 13:11:10 +0900

    feat: replace Typo component with SectionTitle in HomeActivitySection for consistent header styling

--- src/sections/home/Activity/index.tsx
+++ src/sections/home/Activity/index.tsx
@@ -3,16 +3,12 @@ import { weight } from "@/styles/fonts/values/weight";
 
 import * as s from "./style.css";
 import ActivityItemList from "./ActivityItemList";
+import SectionTitle from "@/components/SectionTitle";
 
 export default function HomeActivitySection() {
   return (
     <section className={s.base}>
-      <Typo as="h2" size={{
-        550: 26,
-        default: 32,
-      }} weight={weight.semibold}>
-        동아리 활동
-      </Typo>
+      <SectionTitle>동아리 활동</SectionTitle>
       <div className={s.activityList}>
         <ActivityItemList />
       </div>
