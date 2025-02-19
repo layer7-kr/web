7a2d2c8ed38bda9fcfa230c9971bde1bbd551222
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-19 22:17:12 +0900

    feat: update metadata in layout for improved SEO and localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,29 @@ import '@/styles/global.css';
 
 export const metadata: Metadata = {
   title: 'Layer7',
-  description: 'Layer7',
+  description: '선린인터넷고등학교 해킹 전공 동아리',
+  keywords: [
+    'Layer7',
+    'layer7',
+    '레이어세븐',
+    '선린인터넷고등학교',
+    'sunrin',
+    '선린',
+    '정보보호과',
+    '동아리',
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    url: 'https://new.layer7.kr',
+    siteName: 'Layer7',
+    description: '선린인터넷고등학교 해킹 전공 동아리',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/tapie-kr/.github/assets/cover.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
