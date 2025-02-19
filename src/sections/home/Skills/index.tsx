49cfc835b088c9ec6de0dd8d0e80e16758077ba2
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2025-02-19 20:41:35 +0900

    feat: remove mobile-specific padding adjustments in HomeSkillsSection for consistent layout

--- src/sections/home/Skills/index.tsx
+++ src/sections/home/Skills/index.tsx
@@ -1,14 +1,11 @@
 import SectionTitle from '@/components/SectionTitle';
-import { useMediaQuery } from '@/lib/hooks/useMediaQuery';
 import CurriculumItemList from './CurriculumList';
 import SkillTag from './SkillTag';
 import * as s from './style.css';
 
 export default function HomeSkillsSection() {
-  const isMobile = useMediaQuery('(max-width: 768px)');
-
   return (
-    <section className={s.base} style={{ paddingTop: isMobile ? 0 : 80 }}>
+    <section className={s.base}>
       <div className={s.skills}>
         <SectionTitle>Skills</SectionTitle>
         <div className={s.skillTags}>
@@ -19,10 +16,7 @@ export default function HomeSkillsSection() {
           <SkillTag name={'리버스 엔지니어링'} order={5} />
         </div>
       </div>
-      <div
-        className={s.curriculum}
-        id={'curriculum'}
-        style={{ paddingTop: isMobile ? 80 : 0 }}>
+      <div className={s.curriculum} id={'curriculum'}>
         <SectionTitle>커리큘럼</SectionTitle>
         <div>
           <CurriculumItemList />
