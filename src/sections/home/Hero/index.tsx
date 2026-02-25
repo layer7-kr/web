86e408959ccbfc5b1599b88b1b7c74e2ba0f5403
shxz9u <qpxjfdl@naver.com>
2026-02-26 03:10:47 +0900

    add: add preview URL

--- src/sections/home/Hero/index.tsx
+++ src/sections/home/Hero/index.tsx
@@ -37,8 +37,12 @@ export default function HomeHeroSection() {
           {/* <ApplyButton active href='https://ssf.sunrin.io/camp/layer7'>
             2025 소프트웨어나눔축제
           </ApplyButton> */}
+          <ApplyButton active href='https://preview.layer7.kr/'>
+            2026 시연회
+          </ApplyButton>
         </div>
       </div>
-    </section>
+    </div>
+    </section >
   );
 }
